ebb15c7dbd3968013c80524e96574cac810be0f1
Santiago Almejun <santi.alme@gmail.com>
2020-07-08 12:45:24 -0300

    added validator module

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,19 @@
 extern crate rand;
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs::{OpenOptions};
+use std::io::{Write};
 use std::error::Error;
+
+mod validator;
 mod game;
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let expected_arguments = 2;
+	let (players, debug) = validator::validate_arguments()?;
 
-    let args: Vec<String> = env::args().collect();
+    println!("Cantidad de jugadores: {:?}", players);
+    println!("Modo de ejecución en debug: {:?}", debug);
 
-    if args.len() != (expected_arguments + 1) {
-    	Err("Number of players and run mode must be specified")?;
-    }
-
-    let players_amount:i32 = match args[1].parse::<i32>() {
-        Ok(players_amount)  => players_amount,
-        Err(e) => return Err(Box::new(e)),
-    };
-    if players_amount < 4 || players_amount % 2 != 0 {
-        Err("Number of players must be even and at least 4")?;
-    }
-    let debug_mode = args[2] == String::from("debug");
-
-    println!("Cantidad de jugadores: {:?}", players_amount);
-    println!("Modo de ejecucion en debug: {:?}", debug_mode);
-
-    let mut file = File::create("log.txt")?;
-    file.write_all(b"Hello, world!")?;
+    let mut file = OpenOptions::new().append(true).create(true).open("log.txt")?;
+    writeln!(&mut file, "Hello, world!")?;
 
     let mut deck = game::create_deck();
     game::shuffle_deck(&mut deck);
---
+++ src/validator.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::error::Error;
+
+pub fn validate_arguments() -> Result<(i32,bool), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let expected_arguments = 2;
+
+    if args.len() != (expected_arguments + 1) {
+        Err("Number of players and debug mode must be specified")?;
+    }
+
+    let players: i32 = match args[1].parse::<i32>() {
+        Ok(players)  => players,
+        Err(e) => return Err(Box::new(e)),
+    };
+
+    if players < 4 || players % 2 != 0 {
+        Err("Number of players must be even and at least 4")?;
+    }
+
+    let debug = args[2] == String::from("debug");
+    std::result::Result::Ok((players, debug))
+}
